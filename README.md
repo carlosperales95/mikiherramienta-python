65c76cdf0d29aa330c85901c05fe6dc700068841
carlosperales95 <carlos.perales95@opendeusto.es>
2024-10-31 18:45:36 +0100

    Mejora de menu y readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# mikiherramienta-python
+# La Mikiherramienta
+Este proyecto empezo como 3 diferentes scripts que hicimos en directo.
+
+Al final los hemos unido en una sola aplicacion, y lo hemos dockerizado para facilitar la instalacion y el setup de este proyectillo.
+
+## Como usarlo
+
+Primero hay que clonar el repositorio:
+```
+git clone https://github.com/carlosperales95/mikiherramienta-python
+```
+
+Una vez tengamos los archivos clonados, hay dos opciones para instalar/ejecutar las dependencias y codigo.
+
+### Virtual Environment (venv)
+Los siguientes comandos nos ayudaran a crear y activar un entorno virtual para las depencias del proyecto.
+
+Al sustituir `<nombre>` por el nombre que deseamos para nuestra carpeta de entorno virtual, los comandos estaran listos para su ejecucion:
+```
+python -m venv <nombre>
+source /<nombre>/bin/activate
+```
+
+Una vez activado, podemos instalar las dependencias del archivo `requirements.txt` en nuestro entorno usando `pip`:
+```
+pip install -r ./requirements.txt
+<!-- chmod +x main.py -->
+```
+
+Ya estamos listos para ejecutar el programa:
+```
+python main.py
+```
+
+### Docker
+
+Para ejecutar nuestro programa en un entorno containerizado, debemos instalar `docker`. Esta informacion se puede encontrar en la documentacion oficial de [Dockerdocs](https://docs.docker.com/engine/install/).
+
+Una vez instalado, debemos crear la imagen para poder montarla en contenedores. COmo ejemplo de nombre de imagen he elegido `mikiherramienta`, pero este nombre puede ser cambiado:
+```
+docker build -t mikiherramienta
+```
+
+Una vez nuestra imagen ha sido creada con exito, podemos usarla para crear un nuevo contenedor:
+```
+docker run -i -t mikiherramienta
+```
--- main.py
+++ main.py
@@ -3,41 +3,60 @@ import bot
 import gen_pass as gp
 import os
 
+# Funcion de input para volver al menu
 def back_to_menu():
     u_input = input("\nHaz click en cualquier tecla para volver al menu: ")
     os.system("clear")
     return u_input
 
+# Funcion de print del menu y opciones
+def print_menu(options):
+    print("######## MIKIHERRAMIENTA ########\n")
+    for idx, o in enumerate(options):
+        print(f"{idx + 1}: {o}")
 
-while True:
-    print("######## MIKIHERRAMIENTA ########")
-    print("Que herramienta quieres usar? (0 para salir)")
-    print("1) Chat con Bot")
-    print("2) Generar contraseñas")
-    print("3) Prueba de internet")
-    menu_input = input()
+# Funcion de peticion de input y validacion de opciones
+def ask_input(options):
+    while True:
+        menu_input = input("Que herramienta quieres usar? (0 para salir): ")
+        try:
+            converted_input = int(menu_input)
+            if converted_input < 0 or converted_input > len(options):
+                print(f"\nEl numero: {converted_input} no es una opcion valida!\n")
+                continue
+            return converted_input
+        except:
+            print("\nOye eso no es un numero! Intentalo de nuevo\n")
+            continue
 
-    try:
-        converted_input = int(menu_input)
-    except:
-        print("\nOye eso no es un numero!")
-        continue
+def main():
+    while True:
+        options = ["Chat con Bot", "Generar contraseñas", "Prueba de internet"]
+        print_menu(options)
 
-    if converted_input == 0:
-        print("\nSaliendo del programa...")
-        break
-    
-    if converted_input == 1:
-        bot.run_bot()
-        if back_to_menu():
-            continue
-    
-    if converted_input == 2:
-        gp.give_me_five_pass()
-        if back_to_menu():
-            continue
-    
-    if converted_input == 3:
-        it.test_my_speed()
-        if back_to_menu():
-            continue
+        # Pedir y validar input
+        selected_option = ask_input(options)
+
+        # Ejecutar en base al input
+        if selected_option == 0:
+            print("\nSaliendo del programa...")
+            break
+        
+        if selected_option == 1:
+            bot.run_bot()
+            if back_to_menu():
+                continue
+        
+        if selected_option == 2:
+            gp.give_me_five_pass()
+            if back_to_menu():
+                continue
+        
+        if selected_option == 3:
+            it.test_my_speed()
+            if back_to_menu():
+                continue
+
+# Main func
+if __name__ == "__main__":
+    main()
